b67bc8a912cd9acbcd9d4ec28aa7bb25772d03db
marcmascarell <9d56Zkeql4Vq6zwHABop>
2018-05-06 09:04:43 +0200

    Fix method order

--- src/bot.ts
+++ src/bot.ts
@@ -13,25 +13,35 @@ const getClient = () => client
 const init = () => {
     client.on('ready', async () => {
         client.registry
-        // Registers your custom command groups
+            .registerDefaultTypes()
+            .registerDefaultGroups()
+            .registerDefaultCommands({
+                help: true
+            })
+            // Registers all built-in groups, commands, and argument types
+            // .registerDefaults()
+
+            // Registers your custom command groups
             .registerGroups([
                 ['match', 'Matchmaking commands'],
                 // ['some', 'Some group'],
                 // ['other', 'Some other group']
             ])
 
-            // Registers all built-in groups, commands, and argument types
-            // .registerDefaults()
-            .registerDefaultCommands({
-                help: true
-            })
-            .registerDefaultGroups()
-            .registerDefaultTypes()
-
             .registerTypesIn(path.join(__dirname, 'Types'))
 
             // Registers all of your commands in the ./commands/ directory
             .registerCommandsIn(path.join(__dirname, 'Commands/match'));
+
+        // Create an event listener for new guild members
+        client.on('guildMemberAdd', member => {
+            // Send the message to a designated channel on a server:
+            const channel = member.guild.channels.first();
+            // Do nothing if the channel wasn't found on this server
+            if (!channel) return;
+            // Send the message, mentioning the member
+            channel.send(`Welcome ${member}! Use \`!help\`.`);
+        });
     })
 
     client.login(secrets.discordToken);
