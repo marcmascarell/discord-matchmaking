fbb7fb376622d7f4f5e2c1cad4112081deb869c3
Marc Mascarell <marcmascarell@gmail.com>
2018-07-23 19:43:53 +0200

    Revert "Process activity"

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,24 +1,10 @@
 import Model from './BaseModel'
 
 export default class User extends Model {
-    public id: number
-    public discord_id: string // Discord ids are strings
-    public discord_username: string
-    public discord_discriminator: string
-    public discord_avatar: string
-
-    public discordId: string
-    public discordUsername: string
-    public discordDiscriminator: string
-    public discordAvatar: string
-
-    static get virtualAttributes() {
-        return ['username']
-    }
-
-    get username() {
-        return this.discordUsername
-    }
+    public id: string // Discord ids are strings
+    public username: string
+    public discriminator: string
+    public avatar: string
 
     static get tableName() {
         return 'users';
