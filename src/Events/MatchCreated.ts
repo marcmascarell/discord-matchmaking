78d3facf406971b058e8ed07c4998b943e63f8af
marcmascarell <marcmascarell@gmail.com>
2018-09-19 15:03:23 +0200

    wip

--- src/Events/MatchCreated.ts
+++ src/Events/MatchCreated.ts
@@ -1,11 +1,13 @@
 import Event from './Event'
 import NotifyNewMatch from '../Listeners/NotifyNewMatch'
+import CreateTextChannel from "../Listeners/CreateTextChannel";
 
 export default class MatchCreated extends Event {
 
     get listeners() : Array<any> {
    		return [
-            NotifyNewMatch
+            NotifyNewMatch,
+            CreateTextChannel
         ]
 	}
 
