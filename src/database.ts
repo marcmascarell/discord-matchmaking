ea954882655868ee37e4b8d1d57b6ddbdb0240d8
marcmascarell <9d56Zkeql4Vq6zwHABop>
2018-10-14 18:58:07 +0200

    Notice

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,10 @@ const config = require("../knexfile")
 import utils from "./Utilities/utils"
 
 const init = () => {
+    if (!utils.getEnvironment()) {
+        throw new Error("Missing required NODE_ENV")
+    }
+
     // Initialize knex.
     const knex = Knex(config[utils.getEnvironment()])
 
